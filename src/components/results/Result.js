d0adb1d0cd7c58f179381f0d97bd11c2cf3fb1f6
Kevin <kevin.galasso01@gmail.com>
2021-10-20 12:55:07 +0200

    V1.4 Aggiunta key nel map

--- src/components/results/Result.js
+++ src/components/results/Result.js
@@ -6,7 +6,7 @@ import {Pages} from './pages/Pages'
 export const Result = (props) => {
     const movies = props.movies;
 
-    const movieList = movies.map((movie)=> <MovieCard key={movie.codice} displayMovie={movie} />);
+    const movieList = movies.map((movie)=> <MovieCard key= {movie.codice} displayMovie={movie} />);
 
     return (
         <div className = 'result'>
